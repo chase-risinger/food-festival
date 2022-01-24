b43fc52b567d214f99ecef07ff78e564cb6c779b
chase-risinger <ccr1988@gmail.com>
2022-01-24 16:29:08 -0600

    splitting the code, and image packing

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,52 @@
 // we need to tell it where to find the javascript that we're going to minimize / bundle our code
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'  // tells us its going into directory 'dist' and it will be named main.bundle.js
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return '[path][name].[ext]'; // gives it the same name as the original jpeg
+                            },
+                            publicPath: function (url) {
+                                return url.replace('../', '/assets/');
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery" // we are telling webpack to undertstand that these global variables, jquery and $, belong to jquery
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
-    mode: 'development'
+    mode: 'development',
+
 };
